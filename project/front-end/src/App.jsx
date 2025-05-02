ebdcfba23f6d00cd91475dc9ce28ea73f758d22d
hmachefe <hugo.machefer@gmail.com>
2025-05-02 16:53:07 -0400

    add logo and fix title positionning

--- project/front-end/src/App.jsx
+++ project/front-end/src/App.jsx
@@ -1,20 +1,32 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CreateForm from "./pages/CreateForm";
 import ListPage   from "./pages/ListPage";
 import DetailPage from "./pages/DetailPage";
+import logo       from "./assets/logo.jpeg";
+import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
-        <Link to="/">Create</Link> |{" "}
-        <Link to="/applications">List</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<CreateForm />} />
-        <Route path="/applications" element={<ListPage />} />
-        <Route path="/applications/:id" element={<DetailPage />} />
-      </Routes>
+      <header className="header">
+        <div className="header-inner">
+          <img src={logo} alt="Fairstone Logo" className="logo" />
+          <nav className="nav">
+            <Link to="/">Create</Link>
+            <span className="divider">|</span>
+            <Link to="/applications">List</Link>
+          </nav>
+        </div>
+      </header>
+
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<CreateForm />} />
+          <Route path="/applications" element={<ListPage />} />
+          <Route path="/applications/:id" element={<DetailPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
